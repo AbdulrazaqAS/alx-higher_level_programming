334940df70467417cdbecf503c932b91fb43980a
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-12-04 07:58:58 -0800

    Py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print('Last digit of', number, 'is', end = " ")
+print('Last digit of', number, 'is', end=" ")
 sign = -1 if number < 0 else 1
 number = abs(number) % 10
 number *= sign
