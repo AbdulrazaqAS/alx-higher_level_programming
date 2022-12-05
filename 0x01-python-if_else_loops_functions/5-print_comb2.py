d6d550b58c3004b865c91001815de59dc8f5f19f
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-12-05 02:34:06 -0800

    Py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for var in range(0, 100):
-    print('{:02d}'.format(var), end=', ')
-print()
+    if var == 99:
+        print('99')
+    else:
+        print('{:02d}'.format(var), end=', ')
