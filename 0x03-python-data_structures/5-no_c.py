ca5d3725549ec6d60a2252e8baaabed16bab0259
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-12-12 06:16:40 -0800

    Py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-	new = ''
-	q
+    new = ''
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        new += char
+    return new
