1f9860dad678c180c61ba358792a5ff77bb879a5
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-12-13 13:49:46 -0800

    Py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman = {'I' : 1, 'II' : 2, 'III' : 3, 'IV' : 4, 'V' : 5,
-            'VI' : 6, 'VII' : 7, 'VIII' : 8, 'IX' : 9, 'X' : 10,
-            'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    if (roman_string is none or int(roman_string):
+        return 0
     res, last = 0, 0
     for i in roman_string:
         if roman[i] > last:
