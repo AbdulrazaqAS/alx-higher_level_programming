7468fa76a5af5a049de521dcafd2f63f6f915526
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2022-12-26 06:06:10 -0800

    Py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     max = 0
     key = ''
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
 
     for i in a_dictionary:
